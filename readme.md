607179d7b5b5746afd25c145d1c2a202efd59089
TheTechdoodle <admin+github@darkender.com>
2020-04-10 23:08:32 -0700

    Add code

---
+++ readme.md
@@ -0,0 +1,20 @@
+This plugin makes all projectiles fired by players and dispensers accurate (without random spread)
+
+Arrows from a bow (close range) without / with the plugin
+![Screenshot](https://i.imgur.com/h5bVaAM.png)
+
+Arrows fired from a dispenser without / with the plugin
+![Screenshot](https://i.imgur.com/lxJR6YS.png)
+
+Thrown tridents without / with the plugin
+![Screenshot](https://i.imgur.com/P9kXly9.png)
+
+The following projectiles are more accurate:
+ - Arrows from bows
+ - Arrows from crossbows
+ - Arrows from dispensers
+ - Tridents
+ - Splash / Lingering potions
+ - Enderpearls
+
+Note that crossbows with multishot are not currently corrected. 
---
+++ src/main/java/com/darkender/plugins/accuracy/Accuracy.java
@@ -0,0 +1,69 @@
+package com.darkender.plugins.accuracy;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.projectiles.BlockProjectileSource;
+import org.bukkit.projectiles.ProjectileSource;
+import org.bukkit.util.Vector;
+
+public class Accuracy extends JavaPlugin implements Listener
+{
+    @Override
+    public void onEnable()
+    {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    @EventHandler
+    public void onProjectileFire(ProjectileLaunchEvent event)
+    {
+        ProjectileSource source = event.getEntity().getShooter();
+        if(source instanceof Player)
+        {
+            Player p = (Player) source;
+            
+            // Ignore multishot crossbows
+            if(!isAllowedSource(p.getInventory().getItemInMainHand()) || !isAllowedSource(p.getInventory().getItemInOffHand()))
+            {
+                return;
+            }
+            
+            fixVelocity(event.getEntity(), p.getEyeLocation().getDirection());
+        }
+        else if(source instanceof BlockProjectileSource)
+        {
+            // There's a better way to do this for sure
+            // Right now it's comparing the entity's block with the source block to form a normal vector
+            fixVelocity(event.getEntity(),
+                    ((BlockProjectileSource) source).getBlock().getFace(event.getEntity().getLocation().getBlock()).getDirection());
+        }
+    }
+    
+    private boolean isAllowedSource(ItemStack item)
+    {
+        if(item == null)
+        {
+            return true;
+        }
+        
+        if(item.getType() == Material.CROSSBOW && item.getEnchantmentLevel(Enchantment.MULTISHOT) != 0)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    private void fixVelocity(Entity entity, Vector direction)
+    {
+        double current = entity.getVelocity().length();
+        entity.setVelocity(direction.clone().normalize().multiply(current));
+    }
+}
